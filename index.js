3b8e1e12bccdd06343c293280adf677e60f373d8
Sai Charan <charankuppa@gmail.com>
2016-06-10 21:35:01 +0530

    Generates critical css for a URL and a HTML string

--- index.js
+++ index.js
@@ -7,15 +7,28 @@ var port = 8080;
 app.use(bodyParser.text({type: 'text/html'}));
 
 app.post('/api/critical', (req, res) => {
-	console.log(req.body);
+	var options = JSON.parse(req.body);
+	console.log("the options are", options);
+	if(options.url) {
+			critical.generate({
+	   	src: options['url'],
+	    width: 1300,
+	    height: 900,
+	    minify: true
+		}).then((criticalCss) => {
+			res.send(criticalCss);
+		});
+	}
+	else if (options.html) {
 	critical.generate({
-   	src: req.body,
-    width: 1300,
-    height: 900,
-    minify: true
-	}).then((criticalCss) => {
-		res.send(criticalCss);
-	});
+	   	html: options['html'],
+	    width: 1300,
+	    height: 900,
+	    minify: true
+		}).then((criticalCss) => {
+			res.send(criticalCss);
+		});
+	}
 });
 
 app.listen(port, () => {
