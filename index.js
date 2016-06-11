b9234da5f27b6b0f86b9e183fb02daad2f0091d1
Tejas Dinkar <tejas@gja.in>
2016-06-11 23:16:27 +0530

    Adhering to content type strictly

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var critical = require('critical');
+var _ = require("lodash");
+
 var app = express();
 var port = 8080;
 
 app.use(bodyParser.text({type: 'text/html'}));
+app.use(bodyParser.json({type: 'application/json'}));
 
 app.post('/api/critical', (req, res) => {
-	var options = JSON.parse(req.body);
-	console.log("the options are", options);
-	if(options.url) {
-			critical.generate({
-	   	src: options['url'],
-	    width: 1300,
-	    height: 900,
-	    minify: true
-		}).then((criticalCss) => {
-			res.send(criticalCss);
-		});
-	}
-	else if (options.html) {
-	critical.generate({
-	   	html: options['html'],
-	    width: 1300,
-	    height: 900,
-	    minify: true
-		}).then((criticalCss) => {
-			res.send(criticalCss);
-		});
-	}
+  var options = req.body;
+  if(req.headers["content-type"] == "text/html")
+    options = {html: req.body};
+
+  options = _.chain({})
+    .extend(options, req.query)
+    .pick(["html", "url"])
+    .value();
+
+  critical.generate(_.extend({
+    width: 1300,
+    height: 900,
+    minify: true
+  }, options)).then((criticalCss) => {
+    res.send(criticalCss);
+  });
 });
 
 app.listen(port, () => {
-	console.log("Listening on port 8080!");
+  console.log("Listening on port 8080!");
 });
